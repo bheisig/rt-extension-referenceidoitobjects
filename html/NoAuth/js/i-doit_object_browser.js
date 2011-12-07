e2a8f324b85dfeba2a27ca5e60b5d0ecdeafc90a
Leonard Fischer <lfischer@synetics.de>
2011-12-07 17:30:55 +0100

    Updated some stuff

--- html/NoAuth/js/i-doit_object_browser.js
+++ html/NoAuth/js/i-doit_object_browser.js
@@ -381,7 +381,8 @@
 							selected = true;
 						}
 					
-						div.append('<br /><input type="checkbox" style="margin-left:' + (span + 20) +'px;" value="' + e.id + '" name="i-doit-treebrowser-obj[]" ' + ((selected) ? 'checked="checked"' : '') + '/> ' + e.related_object_title);
+						div.append('<br /><input type="checkbox" style="margin-left:' + (span + 20) +'px;" value="' + e.id + '" name="i-doit-treebrowser-obj[]" ' + ((selected) ? 'checked="checked"' : '') + '/> ' + 
+							'<span class="obj-name">' + e.related_object_title) + '</span> (<span class="obj-type">' + e.type_title + '</span>)';
 					});
 				} else {
                   window.error_notice('<% loc("Error while loading relation objects") %>');
